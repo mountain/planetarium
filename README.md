975ee1fbf1bc8bc9bdb0ba406026e6ad72dd1312
mingli <mingli.yuan@gmail.com>
2018-03-20 15:11:19 +0800

    make residual work

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # planetarium
-planetarium
+
+planetarium is a project try to solve the math problem beneath planet finding by deeplearning technologies.
+
+![Observation][obsv]
+
+![Predication][pred]
+
+
+[obsv]: https://github.com/mountain/planetarium/raw/master/obsv.png
+[pred]: https://github.com/mountain/planetarium/raw/master/pred.png
+
+
--- src/planets.py
+++ src/planets.py
@@ -11,8 +11,6 @@ from os import environ
 
 xp = np
 if environ.get('CUDA_HOME') is not None:
-    import cupy as cp
-
     xp = np
 
 import torch as th
@@ -21,6 +19,11 @@ import torch.optim as optim
 
 from torch.autograd import Variable
 
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+
 from flare.nn.mlp import MLP
 from flare.nn.residual import ResidualBlock1D
 from flare.nn.vae import VAE, vae_loss
@@ -28,34 +31,45 @@ from flare.nn.vae import VAE, vae_loss
 from flare.learner import StandardLearner
 from flare.dataset.decorators import attributes, segment, divid, sequential, data
 
+
+epsilon = 0.00001
+
+SCALE = 50.0
+MSCALE = 500.0
+
 BATCH = 5
-SIZE = 12
-WINDOW = 6
+SIZE = 36
+WINDOW = 12
 INPUT = 4
 OUTPUT = 2
 
 
-def transform(k, x):
-    return x / 150.0
+mass = None
 
+
+def transform(k, x):
+    return x / SCALE
 
 def generator(n, m, yrs):
+    global mass
+
     n = int(n)
     m = int(m)
     pv = int(n + 1)
     sz = int(n + m + 1)
     szn = int(3 * n)
     szm = int(3 * m)
-    m = xp.random.rand(sz) * 0.001
-    m[0] = 1.0
+    mass = xp.array(xp.random.rand(sz) / MSCALE, dtype=np.float)
+    mass[0] = 1.0
 
-    x = 75.0 * xp.random.rand(sz, 3)
+    x = SCALE / 2.0 * (2 * xp.random.rand(sz, 3) - 1)
     x[0, :] = xp.array([0.0, 0.0, 0.0], dtype=xp.float64)
 
-    v = xp.sqrt(au.G) * xp.random.rand(sz, 3)
-    v[0] = - np.sum((m[1:, np.newaxis] * v[1:]) / m[0], axis=0)
+    r = xp.sqrt(x[:, 0] * x[:, 0] + x[:, 1] * x[:, 1] + x[:, 2] * x[:, 2] + epsilon).reshape([INPUT + OUTPUT + 1, 1])
+    v = xp.sqrt(au.G / r) * (2 * xp.random.rand(sz, 3) - 1)
+    v[0] = - np.sum((mass[1:, np.newaxis] * v[1:]) / mass[0], axis=0)
 
-    solver = ode.verlet(nbody.acceleration_of(au, m))
+    solver = ode.verlet(nbody.acceleration_of(au, mass))
 
     t = 0
     lastyear = 0
@@ -65,11 +79,6 @@ def generator(n, m, yrs):
         if year != lastyear:
             lastyear = year
             rtp = transform(sz, x)
-            #print('----------------------------------------')
-            #print(year)
-            #print(np.max(rtp), np.min(rtp), np.average(rtp))
-            #print(np.max(v), np.min(v), np.average(v))
-            #print('----------------------------------------')
             input = rtp[1:pv].reshape(szn)
             output = rtp[pv:sz].reshape(szm)
             yield year, input, output
@@ -108,11 +117,11 @@ class Model(nn.Module):
             ResidualBlock1D(2187),
             ResidualBlock1D(2187),
             Permutation(),
-            MLP(dims=[2187, WINDOW * (INPUT + OUTPUT) * 3]),
+            MLP(dims=[2187, WINDOW * (INPUT + OUTPUT) * 3 + INPUT + OUTPUT]),
         )
 
         self.evolve = nn.Sequential(
-            MLP(dims=[12, 361]),
+            MLP(dims=[7 * (INPUT + OUTPUT), 361]),
             Permutation(),
             ResidualBlock1D(361),
             ResidualBlock1D(361),
@@ -120,9 +129,9 @@ class Model(nn.Module):
             ResidualBlock1D(361),
             ResidualBlock1D(361),
             Permutation(),
-            MLP(dims=[361, 12]),
+            MLP(dims=[361, 6 * (INPUT + OUTPUT)]),
         )
-        self.vae = VAE(WINDOW * (INPUT + OUTPUT) * 3, 48, 12)
+        self.vae = VAE(WINDOW * (INPUT + OUTPUT) * 3, 90, 6 * (INPUT + OUTPUT))
 
         self.zero = th.zeros(1)
         self.zeros = th.zeros([BATCH, 1, SIZE * OUTPUT * 3])
@@ -134,19 +143,6 @@ class Model(nn.Module):
 
         self.error = Variable(self.zero.clone())
         self.divrg = Variable(self.zero.clone())
-        self.ratio = Variable(th.cat([ones, ones, ones, ones, ones, ones,
-                                      ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0,
-                                      ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0,
-                                      ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0,
-                                      ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0,
-                                      ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0,
-                                      ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0,
-                                      ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0,
-                                      ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0,
-                                      ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0,
-                                      ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0,
-                                      ones, ones, ones, ones, ones, ones
-                                      ], dim=2), requires_grad=False)
 
     def batch_size_changed(self, new_val, orig_val):
         self.batch = new_val
@@ -158,94 +154,47 @@ class Model(nn.Module):
             ones = ones.cuda()
             self.zeros = self.zeros.cuda()
 
-        self.ratio = Variable(th.cat([ones, ones, ones, ones, ones, ones,
-                                      ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0,
-                                      ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0,
-                                      ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0,
-                                      ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0,
-                                      ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0,
-                                      ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0, ones / 6.0,
-                                      ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0, ones / 5.0,
-                                      ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0, ones / 4.0,
-                                      ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0, ones / 3.0,
-                                      ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0, ones / 2.0,
-                                      ones, ones, ones, ones, ones, ones
-                                      ], dim=2), requires_grad=False)
-
     def forward(self, x):
         pivot = WINDOW * INPUT * 3
         final = WINDOW * (INPUT + OUTPUT) * 3
         estm = None
 
         result_p = Variable(self.zeros.clone())
+        self.merror = Variable(self.zero.clone())
         self.error = Variable(self.zero.clone())
         self.divrg = Variable(self.zero.clone())
         for i in range(SIZE - WINDOW):
             start = i * INPUT * 3
             end = (i + WINDOW) * INPUT * 3
             input = x[:, :, start:end]
-            cur = self.guess(input)
-            gss = cur.clone()
-            inner_mu, inner_logvar = self.vae.encode(cur)
+
+            guess = self.guess(input)
+            gmass = guess[:, :, 0:(INPUT + OUTPUT)] / MSCALE
+            cur = guess[:, :, (INPUT + OUTPUT):]
+            self.gmass = gmass
+
+            inner_mu, inner_logvar = self.vae.encode((cur + 1) / 2)
             inner_cur = self.vae.reparameterize(inner_mu, inner_logvar)
-            outer_cur = self.vae.decode(inner_cur)
-            self.divrg += vae_loss(self.batch, final, outer_cur, Variable(cur.data, requires_grad=False), inner_mu,
+            outer_cur = 2 * self.vae.decode(inner_cur) - 1
+            self.divrg += vae_loss(self.batch, final, (outer_cur + 1) / 2, (Variable(cur.data, requires_grad=False) + 1) / 2, inner_mu,
                                    inner_logvar)
 
-            inner_nxt = self.evolve(inner_cur)
-            nxt = self.vae.decode(inner_nxt)
+            inner_nxt = self.evolve(th.cat([gmass, inner_cur], dim=2))
+            nxt = 2 * self.vae.decode(inner_nxt) - 1
 
             if estm is None:
                 output = cur[:, :, pivot:final]
             else:
                 output = estm[:, :, pivot:final]
                 self.error += mse(estm, Variable(cur.data, requires_grad=False))
+                self.merror += mse(gmass[0, 0], Variable(th.from_numpy(mass[1:]).float(), requires_grad=False)) * MSCALE * MSCALE
 
             start = i * OUTPUT * 3
             end = (i + WINDOW) * OUTPUT * 3
-            result_p[:, :, start:end] = result_p[:, :, start:end] + output + gss[:, :, start:end]
+            result_p[:, :, start:end] = (output + cur[:, :, start:end]) / 2
             estm = nxt
 
-        result_n = Variable(self.zeros.clone())
-        self.error = Variable(self.zero.clone())
-        self.divrg = Variable(self.zero.clone())
-        for i in range(SIZE - WINDOW, 0, -1):
-            start = i * INPUT * 3
-            end = (i + WINDOW) * INPUT * 3
-            order = [
-                69, 70, 71, 66, 67, 68, 63, 64, 65, 60, 61, 62, 57, 58, 59, 54, 55, 56,
-                51, 52, 53, 48, 49, 50, 45, 46, 47, 42, 43, 44, 39, 40, 41, 36, 37, 38,
-                33, 34, 35, 30, 31, 32, 27, 28, 29, 24, 25, 26, 21, 22, 23, 18, 19, 20,
-                15, 16, 17, 12, 13, 14,  9, 10, 11,  6,  7,  8,  3,  4,  5,  0,  1,  2,
-            ]
-            input = x[0, 0][order].view(1, 1, end - start)
-            cur = self.guess(input)
-            gss = cur.clone()
-            inner_mu, inner_logvar = self.vae.encode(cur)
-            inner_cur = self.vae.reparameterize(inner_mu, inner_logvar)
-            outer_cur = self.vae.decode(inner_cur)
-            self.divrg += vae_loss(self.batch, final, outer_cur, Variable(cur.data, requires_grad=False), inner_mu,
-                                   inner_logvar)
-
-            inner_nxt = self.evolve(inner_cur)
-            nxt = self.vae.decode(inner_nxt)
-
-            if estm is None:
-                output = cur[:, :, pivot:final]
-            else:
-                output = estm[:, :, pivot:final]
-                self.error += mse(estm, Variable(cur.data, requires_grad=False))
-
-            start = i * OUTPUT * 3
-            end = (i + WINDOW) * OUTPUT * 3
-            order = [
-                33, 34, 35, 30, 31, 32, 27, 28, 29, 24, 25, 26, 21, 22, 23, 18, 19, 20,
-                15, 16, 17, 12, 13, 14,  9, 10, 11,  6,  7,  8,  3,  4,  5,  0,  1,  2,
-            ]
-            result_n[0, 0, start:end] = result_n[0, 0, start:end] + output[0, 0][order] + gss[0, 0, start:end][order]
-            estm = nxt
-
-        return (result_p + result_n) * self.ratio / 2.0
+        return result_p
 
 
 mse = nn.MSELoss()
@@ -255,18 +204,51 @@ optimizer = optim.Adam(model.parameters(), lr=1)
 
 
 def predict(xs):
-    return model(xs)
+    result = model(xs)
+    return result
+
+
+counter = 0
 
 
 def loss(xs, ys, result):
+    global counter
+    counter = counter + 1
+
     lss = mse(result, ys)
     print('-----------------------------')
-    print(th.max(lss.data), th.min(lss.data), th.mean(lss.data))
-    print(th.max(model.error.data), th.min(model.error.data), th.mean(model.error.data))
-    print(th.max(model.divrg.data), th.min(model.divrg.data), th.mean(model.divrg.data))
+    print('loss:', th.max(lss.data))
+    print('error:', th.max(model.error.data))
+    print('divrg:', th.max(model.divrg.data))
+    print('merror:', th.max(model.merror.data))
     print('-----------------------------')
     sys.stdout.flush()
-    return lss + model.error + model.divrg
+
+    if counter % 1 == 0:
+        input = xs.data.numpy().reshape([SIZE, INPUT, 3])
+        truth = ys.data.numpy().reshape([SIZE, OUTPUT, 3])
+        guess = result.data.numpy().reshape([SIZE, OUTPUT, 3])
+        gmass = model.gmass[0, 0, :].data.numpy()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(input[:, 0, 0], input[:, 0, 1], input[:, 0, 2], 'o', markersize=mass[1] * 3000)
+        ax.plot(input[:, 1, 0], input[:, 1, 1], input[:, 1, 2], 'o', markersize=mass[2] * 3000)
+        ax.plot(input[:, 2, 0], input[:, 2, 1], input[:, 2, 2], 'o', markersize=mass[3] * 3000)
+        ax.plot(input[:, 3, 0], input[:, 3, 1], input[:, 3, 2], 'o', markersize=mass[4] * 3000)
+        plt.savefig('data/obsv.png')
+        plt.close()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(truth[:, 0, 0], truth[:, 0, 1], truth[:, 0, 2], 'ro', markersize=mass[5] * 3000)
+        ax.plot(truth[:, 1, 0], truth[:, 1, 1], truth[:, 1, 2], 'bo', markersize=mass[6] * 3000)
+        ax.plot(guess[:, 0, 0], guess[:, 0, 1], guess[:, 0, 2], 'r+', markersize=gmass[4] * 3000)
+        ax.plot(guess[:, 1, 0], guess[:, 1, 1], guess[:, 1, 2], 'b+', markersize=gmass[5] * 3000)
+        plt.savefig('data/pred.png')
+        plt.close()
+
+    return lss + model.error + model.divrg + model.merror
 
 
 learner = StandardLearner(model, predict, loss, optimizer, batch=BATCH)
