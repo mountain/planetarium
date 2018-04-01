eaac9104b22891233aad605843b7190a887bcd58
mingli <mingli.yuan@gmail.com>
2018-04-02 00:11:35 +0800

    fix size

--- src/planets.py
+++ src/planets.py
@@ -230,7 +230,8 @@ class Evolve(nn.Module):
         xs = []
         for i in range(b):
             vec = out[i]
-            u, p, v = th.svd(status[i])
+            e, m = th.symeig(status[i])
+            v = th.mm(th.t(m), m)
             xs.append(th.mv(v, vec))
 
         result = th.cat(xs, dim=0).view(b, c, s, n)
