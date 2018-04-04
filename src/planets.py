a0bcdab0810aabdfb193dc6aee6a87da4298decf
mingli <mingli.yuan@gmail.com>
2018-04-04 12:33:36 +0800

    fix img

--- src/planets.py
+++ src/planets.py
@@ -208,7 +208,7 @@ def dataset():
 
 
 class Guess(nn.Module):
-    def __init__(self, num_classes=4 * WINDOW * OUTPUT):
+    def __init__(self, num_classes=3 * WINDOW * OUTPUT):
         super(Guess, self).__init__()
 
         self.lstm = ConvLSTM(4 * WINDOW * INPUT, num_classes, 1, padding=0, bsize=REPEAT*BATCH, width=1, height=1)
@@ -220,13 +220,13 @@ class Guess(nn.Module):
         self.lstm.reset()
 
     def forward(self, x):
+        m = x[:, :1, :, :]
         out = x.view(x.size(0), -1, 1, 1)
         out = self.lstm(out)
-        out = out.view(out.size(0), 4, WINDOW, OUTPUT)
-        print('guessm:', th.max(out[:, :1].data), th.min(out[:, :1].data))
+        out = out.view(out.size(0), 3, WINDOW, OUTPUT)
         print('guessx:', th.max(out[:, 1:].data), th.min(out[:, 1:].data))
         sys.stdout.flush()
-        return out
+        return th.cat([m, out], dim=1)
 
 
 class Evolve(nn.Module):
