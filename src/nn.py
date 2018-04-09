74b884c91a1da444cce6ecae4ca75d041de34e28
mingli <mingli.yuan@gmail.com>
2018-04-09 09:22:11 +0800

    fix center

--- src/nn.py
+++ src/nn.py
@@ -190,15 +190,10 @@ class Model(nn.Module):
     def forward(self, x):
         x = x.permute(0, 2, 4, 1, 3).contiguous()
         sr, sb, sc, ss, si = tuple(x.size())
-        state = x.view(sr * sb, sc, ss, si)
-        result = Variable(cast(np.zeros([sr * sb, 8, 2 * SIZE, BODYCOUNT])))
-        for i in range(3):
-            print('-----------------------------')
-            print('idx:', i)
-            sys.stdout.flush()
-
-            state = self.evolve(state, w=SIZE)
-            result[:, :, i*SIZE:(i+1)*SIZE, :] = state[:, :, :, :]
+        state0 = x.view(sr * sb, sc, ss, si)
+        state1 = self.evolve(state0, w=SIZE)
+        state2 = self.evolve(state1, w=SIZE)
+        result = th.cat([state1, state2], dim=2)
 
         return result
 
